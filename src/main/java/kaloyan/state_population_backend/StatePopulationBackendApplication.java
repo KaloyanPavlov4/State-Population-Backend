e7a2cbf3d4b0b28bbcbf849fa85953bde6042d61
Kaloyan <kaloyanpavlov4@gmail.com>
2025-01-21 21:05:01 +0200

    Add main class

--- src/main/java/kaloyan/state_population_backend/StatePopulationBackendApplication.java
+++ src/main/java/kaloyan/state_population_backend/StatePopulationBackendApplication.java
@@ -2,12 +2,13 @@ package kaloyan.state_population_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("kaloyan.state_population_backend.repository")
 public class StatePopulationBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StatePopulationBackendApplication.class, args);
 	}
-
 }
