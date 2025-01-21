c5cd5bb3575979f7548902c3eec3c31ac011e830
Kaloyan <kaloyanpavlov4@gmail.com>
2025-01-21 21:05:10 +0200

    Add repositories

--- src/main/java/kaloyan/state_population_backend/repository/StatePopulationRepository.java
+++ src/main/java/kaloyan/state_population_backend/repository/StatePopulationRepository.java
@@ -1,2 +1,17 @@
-package kaloyan.state_population_backend.repository;public interface StatePopulationRepository {
+package kaloyan.state_population_backend.repository;
+
+import kaloyan.state_population_backend.model.StatePopulation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StatePopulationRepository extends JpaRepository<StatePopulation, String> {
+    @Query(value = "SELECT * FROM state_population_mv", nativeQuery = true)
+    List<StatePopulation> findAllStatePopulations();
+
+    @Query(value = "SELECT * FROM state_population_mv WHERE state = :stateName", nativeQuery = true)
+    StatePopulation findByStateName(String stateName);
 }
