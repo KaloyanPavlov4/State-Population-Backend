68107fb60258f33f73d32bed5b14e66338b76ce8
Kaloyan <kaloyanpavlov4@gmail.com>
2025-01-21 21:04:33 +0200

    Add state enum and converter

--- src/main/java/kaloyan/state_population_backend/utils/State.java
+++ src/main/java/kaloyan/state_population_backend/utils/State.java
@@ -1,20 +1,78 @@
 package kaloyan.state_population_backend.utils;
 
-import jakarta.persistence.Converter;
+import lombok.Getter;
 
-public enum UsState {
-}
+public enum State {
 
-@Converter(autoApply = true)
-public class MyEnumConverter implements AttributeConverter<UsState, String> {
+    ALABAMA("Alabama"),
+    ALASKA("Alaska"),
+    ARIZONA("Arizona"),
+    ARKANSAS("Arkansas"),
+    CALIFORNIA("California"),
+    COLORADO("Colorado"),
+    CONNECTICUT("Connecticut"),
+    DELAWARE("Delaware"),
+    FLORIDA("Florida"),
+    GEORGIA("Georgia"),
+    HAWAII("Hawaii"),
+    IDAHO("Idaho"),
+    ILLINOIS("Illinois"),
+    INDIANA("Indiana"),
+    IOWA("Iowa"),
+    KANSAS("Kansas"),
+    KENTUCKY("Kentucky"),
+    LOUISIANA("Louisiana"),
+    MAINE("Maine"),
+    MARYLAND("Maryland"),
+    MASSACHUSETTS("Massachusetts"),
+    MICHIGAN("Michigan"),
+    MINNESOTA("Minnesota"),
+    MISSISSIPPI("Mississippi"),
+    MISSOURI("Missouri"),
+    MONTANA("Montana"),
+    NEBRASKA("Nebraska"),
+    NEVADA("Nevada"),
+    NEW_HAMPSHIRE("New Hampshire"),
+    NEW_JERSEY("New Jersey"),
+    NEW_MEXICO("New Mexico"),
+    NEW_YORK("New York"),
+    NORTH_CAROLINA("North Carolina"),
+    NORTH_DAKOTA("North Dakota"),
+    OHIO("Ohio"),
+    OKLAHOMA("Oklahoma"),
+    OREGON("Oregon"),
+    PENNSYLVANIA("Pennsylvania"),
+    RHODE_ISLAND("Rhode Island"),
+    SOUTH_CAROLINA("South Carolina"),
+    SOUTH_DAKOTA("South Dakota"),
+    TENNESSEE("Tennessee"),
+    TEXAS("Texas"),
+    UTAH("Utah"),
+    VERMONT("Vermont"),
+    VIRGINIA("Virginia"),
+    WASHINGTON("District of Columbia"),
+    WEST_VIRGINIA("West Virginia"),
+    WISCONSIN("Wisconsin"),
+    WYOMING("Wyoming");
 
-    @Override
-    public String convertToDatabaseColumn(MyEnum attribute) {
-        return attribute != null ? attribute.getCode() : null;
+    @Getter
+    private final String stateName;
+
+    public String getStateName() {
+        return stateName;
     }
 
-    @Override
-    public MyEnum convertToEntityAttribute(String dbData) {
-        return dbData != null ? MyEnum.fromCode(dbData) : null;
+    State(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public static State fromStateName(String stateName) {
+        for (State state : values()) {
+            if (state.getStateName().equalsIgnoreCase(stateName)) {
+                return state;
+            }
+        }
+        throw new IllegalArgumentException("Invalid state name: " + stateName);
     }
 }
+
