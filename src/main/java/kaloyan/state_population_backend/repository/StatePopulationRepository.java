c9ff8dfa5f25af0eaa07f2ffa3102befaeb08518
Kaloyan <kaloyanpavlov4@gmail.com>
2025-01-24 19:44:23 +0200

    Format code

--- src/main/java/kaloyan/state_population_backend/repository/StatePopulationRepository.java
+++ src/main/java/kaloyan/state_population_backend/repository/StatePopulationRepository.java
@@ -4,7 +4,6 @@ import kaloyan.state_population_backend.model.StatePopulation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface StatePopulationRepository extends JpaRepository<StatePopulation, String> {
 
